66b332eb94b7746d4ea27883da23fc90460211f1
ianmorris13 <97080872+ianmorris13@users.noreply.github.com>
2025-11-15 21:55:25 -0800

    parsing csv data with d3

---
+++ assignment-3_mouse-follower/index.js
@@ -0,0 +1,26 @@
+const width = window.innerWidth;
+const height = window.innerHeight;
+const circleRadius = 30;
+const initialMousePosition = { x: width / 2, y: height / 2 };
+
+
+const App = () => {
+    const [mousePosition, setMousePosition] = React.useState(initialMousePosition);
+    const handleMouseMove = React.useCallback(event => {
+        const { clientX, clientY } = event;
+        setMousePosition({ x: clientX, y: clientY });
+    }, [setMousePosition]);
+
+    return (
+        <svg width={width} height={height} onMouseMove={handleMouseMove}>
+            <circle
+                cx={mousePosition.x}
+                cy={mousePosition.y}
+                r={circleRadius}
+            />
+        </svg>
+    )
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
